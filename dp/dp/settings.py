4d1db690dd9af48d64b048308b1712c106c2a692
Sasha-bill <sasha-bill@ya.ru>
2018-04-02 23:10:53 +0300

    add postgresql settings

--- dp/dp/settings.py
+++ dp/dp/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'dp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_db',
-        'USER': 'user_name',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
         'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
