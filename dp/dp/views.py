a6ebe2935ef4802d652ae047fcd181fde5c89693
Sasha-bill <sasha-bill@ya.ru>
2018-04-04 19:27:47 +0300

    modify index.html

--- dp/dp/views.py
+++ dp/dp/views.py
@@ -4,9 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     if request.method == "POST":
+
         name = request.POST.get("name")     # получение значения поля name
         data = request.POST.get("data")     # получение значения поля data
-        return HttpResponse("<h2>Name: {0}  JSON:{1}</h2>".format(data, name))
+        return HttpResponse("<h2>Name: {1}        JSON:{0}</h2>".format(data, name))
     else:
         userform = UserForm()
         return render(request, "index.html", {"form": userform})
